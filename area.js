b4a97c067f3ca35e139d4c509a03e70d7407be47
Puri2000 <160754711+Puri2000@users.noreply.github.com>
2024-02-21 22:20:12 +0530

    Add files via upload

---
+++ area.js
@@ -0,0 +1,9 @@
+let pi=3.14
+let r=8
+let area
+function findArea(){
+    return(pi*r*r)
+}
+area= findArea()
+console.log("Area "+ area)
+
---
+++ temp.js
@@ -0,0 +1,7 @@
+let cel=10
+let fahrenheit
+function celTofar(){
+    return(9/5*cel+32)
+}
+fahrenheit=celTofar()
+console.log(fahrenheit)
